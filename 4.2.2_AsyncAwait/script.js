18a7d7a5f41eac2757f4a5e9bc53c8d6d51856e3
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2025-05-08 20:36:57 -0600

    Se agrega ejemplo de try-catch

--- 4.2.2_AsyncAwait/script.js
+++ 4.2.2_AsyncAwait/script.js
@@ -1,19 +1,30 @@
 console.log(`1. Inicio del script`);
 
+let seCumpleLaPromesa = true;
+
 function iniciarTemporizador(milisegundos) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
             console.log(`Temporizador terminado`);
-            resolve("Promesa cumplida");
+            if(seCumpleLaPromesa){
+                resolve();
+            }else{
+                reject();
+            }
         }, milisegundos);
     });
 }
 
 // async-await -> Una forma de trabajar procesos asíncronos de manera síncrona.
 async function manejarTemporizador() {
-    console.log(`Antes del temporizador`);
-    await iniciarTemporizador(3000); // await requiere sí o sí trabajar con promesas.
-    console.log(`Después del temporizador`);
+    try{
+        console.log(`Antes del temporizador`);
+        await iniciarTemporizador(3000); // await requiere sí o sí trabajar con promesas.
+        console.log(`Después del temporizador`);
+    }catch(e){
+        console.log(`Ocurrió una excepción`);
+        console.log(e);
+    }
 }
 
 manejarTemporizador();
